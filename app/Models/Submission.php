ae1837f46945049e1544be30b25f13efe91dd343
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-19 22:16:45 +0700

    add model Submission and submission table

---
+++ app/Models/Submission.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Submission extends Model
+{
+    protected $table = 'submission';
+
+    protected $fillable = [
+        'tugas_kelas_id',
+        'user_id',
+        'file',
+        'komentar'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2021_05_19_122221_create_submission_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubmissionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submission', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tugas_kelas_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('file')->nullable();
+            $table->string('komentar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submission');
+    }
+}
