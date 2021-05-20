1707c4c0424e4603af687130ceaa46a5bed11353
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-20 19:08:38 +0700

    add KomentarKelasController

---
+++ app/Http/Controllers/KomentarKelasController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KomentarKelas;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class KomentarKelasController extends Controller
+{
+    public function store(Request $request)
+    {
+        KomentarKelas::create(
+            [
+                'komentar'  => $request->komentar,
+                'created_at' => Carbon::now()->setTimezone('Asia/Jakarta')->toDateTimeString(),
+                'updated_at' => Carbon::now()->setTimezone('Asia/Jakarta')->toDateTimeString()
+            ]
+        );
+
+        return response()->json(true, 200);
+    }
+
+    public function destroy(Request $request)
+    {
+        if (!KomentarKelas::where('post_id', $request->post)->where('user_id', $request->user)->exists()) {
+            return response()->json(false, 201);
+        }
+
+        KomentarKelas::where('id', $request->id)->delete();
+
+        return response()->json(true, 201);
+    }
+}
