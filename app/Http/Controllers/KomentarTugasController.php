8d78811e66e558da3fda376dd9065861db6e2b1b
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-20 19:09:12 +0700

    add KomentarTugasController

---
+++ app/Http/Controllers/KomentarTugasController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KomentarTugas;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class KomentarTugasController extends Controller
+{
+    public function store(Request $request)
+    {
+        KomentarTugas::create(
+            [
+                'komentar'  => $request->komentar,
+                'created_at' => Carbon::now()->setTimezone('Asia/Jakarta')->toDateTimeString(),
+                'updated_at' => Carbon::now()->setTimezone('Asia/Jakarta')->toDateTimeString()
+            ]
+        );
+
+        return response()->json(true, 200);
+    }
+
+    public function destroy(Request $request)
+    {
+        if (!KomentarTugas::where('tugas_id', $request->post)->where('user_id', $request->user)->exists()) {
+            return response()->json(false, 201);
+        }
+
+        KomentarTugas::where('id', $request->id)->delete();
+
+        return response()->json(true, 201);
+    }
+}
