bb11fdb7feaa5efaf49d698d859d2bab7ee0792d
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-21 10:40:08 +0700

    update TugasController create submission when tugas created

--- app/Http/Controllers/TugasController.php
+++ app/Http/Controllers/TugasController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\KomentarTugas;
+use App\Models\RoleKelas;
+use App\Models\Submission;
 use App\Models\Tugas;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
@@ -33,39 +36,58 @@ class TugasController extends Controller
             return response()->json(['error' => $validator->errors()]);
         }
 
-        $tugas = Tugas::create(
-            [
-                'kelas_id'  => $request->kelas_id,
-                'user_id'   => $request->id,
-                'nama'      => $request->nama,
-                'deskripsi' => $request->deskripsi,
-                'deadline'  => $request->deadline,
-                'created_at' => Carbon::now()->setTimezone('Asia/Jakarta')->toDateTimeString(),
-                'updated_at' => Carbon::now()->setTimezone('Asia/Jakarta')->toDateTimeString()
-            ]
-        );
+        DB::transaction(function () use ($request) {
+            $tugas = Tugas::create(
+                [
+                    'kelas_id'  => $request->kelas_id,
+                    'user_id'   => $request->id,
+                    'nama'      => $request->nama,
+                    'deskripsi' => $request->deskripsi,
+                    'deadline'  => Carbon::createFromFormat('m-d-Y H:i', $request->deadline)->subHours(7)->format('Y-m-d H:i:s'),
+                    'created_at' => Carbon::now()->setTimezone('Asia/Jakarta')->toDateTimeString(),
+                    'updated_at' => Carbon::now()->setTimezone('Asia/Jakarta')->toDateTimeString()
+                ]
+            );
 
-        if ($request->hasFile('file')) {
-            Tugas::where('id', $tugas->id)
-                ->update(
-                    [
-                        'file'  => $request->file('file')->storeAs($tugas->kelas_id . '/tugas' . '/' . Str::slug($tugas->nama), $request->file('file')->getClientOriginalName())
-                    ]
-                );
-        }
+            $roles = RoleKelas::where('kelas_id', $tugas->kelas_id)->where('role', 3)->get();
 
-        return response()->josn(['error' => null]);
+            if ($roles) {
+                foreach ($roles as $role) {
+                    Submission::create(
+                        [
+                            'tugas_kelas_id' => $tugas->id,
+                            'user_id'        => $role->user_id
+                        ]
+                    );
+                }
+            }
+
+            if ($request->hasFile('file')) {
+                Tugas::where('id', $tugas->id)
+                    ->update(
+                        [
+                            'file'  => $request->file('file')->storeAs($tugas->kelas_id . '/tugas' . '/' . Str::slug($tugas->nama), $request->file('file')->getClientOriginalName())
+                        ]
+                    );
+            }
+        });
+
+
+        return response()->json(['error' => null]);
     }
 
     public function show(Request $request)
     {
         $tugas = Tugas::with('user')->where('id', $request->id)->first();
 
+        $submission = Submission::where('tugas_kelas_id', $tugas->id)->where('user_id', $request->user)->first();
+
         $komentar = KomentarTugas::with('user')->where('tugas_id', $tugas->id)->get();
 
         $response = array(
             'tugas'     => $tugas,
-            'komentar'  => $komentar
+            'komentar'  => $komentar,
+            'submission' => $submission
         );
 
         return response()->json($response, 201);
@@ -102,7 +124,7 @@ class TugasController extends Controller
                 [
                     'nama'          => $request->nama,
                     'deskripsi'     => $request->deskripsi,
-                    'deadline'      => $request->deadline,
+                    'deadline'  => Carbon::createFromFormat('m-d-Y H:i', $request->deadline)->subHours(7)->format('Y-m-d H:i:s'),
                     'updated_at'    => Carbon::now()->setTimezone('Asia/Jakarta')->toDateTimeString()
                 ]
             );
